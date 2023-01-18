917d201b28faf3558e91d35c2025f4bc4000b0df
Bo(b) <lbcsy@yahoo.com.au>
2023-01-18 21:46:04 +1100

    clean u[

--- Src/DongFeng.Launcher/Program.cs
+++ Src/DongFeng.Launcher/Program.cs
@@ -6,16 +6,6 @@
         {
             var baseUrl = "BOOK3:8888/DongFeng-Express";
             
-            //Download Starter files setting
-            var appStarterFiles = new AppStarterFiles()
-            {
-                StartFile = "CustomerMaintenanceNet48.exe",
-                SupportFiles= new string[] {
-                    "CustomerMaintenanceNet48.exe.config",
-                    "JellyFish.dll"
-                }
-            };
-
             new Launcher(baseUrl).Launch();
         }
     }
